a9df0728e573004fd6091733bab733abfe8efa54
Ekapop <eploentham@gmail.com>
2018-12-14 17:52:00 +0700

    Initial commit

---
+++ gui/Form1.Designer.cs
@@ -0,0 +1,308 @@
+﻿namespace bangna_hospital
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.c1Combo1 = new C1.Win.C1List.C1Combo();
+            this.c1DockingTab1 = new C1.Win.C1Command.C1DockingTab();
+            this.c1DockingTabPage1 = new C1.Win.C1Command.C1DockingTabPage();
+            this.c1DateEdit2 = new C1.Win.Calendar.C1DateEdit();
+            this.c1Button1 = new C1.Win.C1Input.C1Button();
+            this.c1DateEdit1 = new C1.Win.C1Input.C1DateEdit();
+            this.c1TextBox1 = new C1.Win.C1Input.C1TextBox();
+            this.c1ThemeController1 = new C1.Win.C1Themes.C1ThemeController();
+            this.c1SuperTooltip1 = new C1.Win.C1SuperTooltip.C1SuperTooltip(this.components);
+            this.c1FlexGrid1 = new C1.Win.C1FlexGrid.C1FlexGrid();
+            this.c1SuperErrorProvider1 = new C1.Win.C1SuperTooltip.C1SuperErrorProvider(this.components);
+            this.c1DockingTab2 = new C1.Win.C1Command.C1DockingTab();
+            this.c1DockingTabPage2 = new C1.Win.C1Command.C1DockingTabPage();
+            this.c1CommandDock1 = new C1.Win.C1Command.C1CommandDock();
+            this.c1List1 = new C1.Win.C1List.C1List();
+            this.c1DockingManager1 = new C1.Win.C1Command.C1DockingManager(this.components);
+            this.c1ThemeController2 = new C1.Win.C1Themes.C1ThemeController();
+            ((System.ComponentModel.ISupportInitialize)(this.c1Combo1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c1DockingTab1)).BeginInit();
+            this.c1DockingTab1.SuspendLayout();
+            this.c1DockingTabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.c1DateEdit2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c1Button1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c1DateEdit1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c1TextBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c1ThemeController1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c1FlexGrid1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c1SuperErrorProvider1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c1DockingTab2)).BeginInit();
+            this.c1DockingTab2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.c1CommandDock1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c1List1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c1DockingManager1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c1ThemeController2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // c1Combo1
+            // 
+            this.c1Combo1.AddItemSeparator = ';';
+            this.c1Combo1.Caption = "";
+            this.c1Combo1.CharacterCasing = System.Windows.Forms.CharacterCasing.Normal;
+            this.c1Combo1.DeadAreaBackColor = System.Drawing.Color.Empty;
+            this.c1Combo1.EditorBackColor = System.Drawing.SystemColors.Window;
+            this.c1Combo1.EditorFont = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.c1Combo1.EditorForeColor = System.Drawing.SystemColors.WindowText;
+            this.c1Combo1.Images.Add(((System.Drawing.Image)(resources.GetObject("c1Combo1.Images"))));
+            this.c1Combo1.Location = new System.Drawing.Point(37, 41);
+            this.c1Combo1.MatchEntryTimeout = ((long)(2000));
+            this.c1Combo1.MaxDropDownItems = ((short)(5));
+            this.c1Combo1.MaxLength = 32767;
+            this.c1Combo1.MouseCursor = System.Windows.Forms.Cursors.Default;
+            this.c1Combo1.Name = "c1Combo1";
+            this.c1Combo1.RowDivider.Style = C1.Win.C1List.LineStyleEnum.None;
+            this.c1Combo1.RowSubDividerColor = System.Drawing.Color.DarkGray;
+            this.c1Combo1.Size = new System.Drawing.Size(121, 21);
+            this.c1Combo1.TabIndex = 0;
+            this.c1Combo1.Text = "c1Combo1";
+            this.c1Combo1.PropBag = resources.GetString("c1Combo1.PropBag");
+            // 
+            // c1DockingTab1
+            // 
+            this.c1DockingTab1.Controls.Add(this.c1DockingTabPage1);
+            this.c1DockingTab1.Location = new System.Drawing.Point(38, 12);
+            this.c1DockingTab1.Name = "c1DockingTab1";
+            this.c1DockingTab1.Size = new System.Drawing.Size(300, 200);
+            this.c1DockingTab1.TabIndex = 1;
+            this.c1DockingTab1.TabsSpacing = 5;
+            // 
+            // c1DockingTabPage1
+            // 
+            this.c1DockingTabPage1.Controls.Add(this.c1DateEdit2);
+            this.c1DockingTabPage1.Controls.Add(this.c1Button1);
+            this.c1DockingTabPage1.Controls.Add(this.c1DateEdit1);
+            this.c1DockingTabPage1.Controls.Add(this.c1TextBox1);
+            this.c1DockingTabPage1.Controls.Add(this.c1Combo1);
+            this.c1DockingTabPage1.Location = new System.Drawing.Point(1, 24);
+            this.c1DockingTabPage1.Name = "c1DockingTabPage1";
+            this.c1DockingTabPage1.Size = new System.Drawing.Size(298, 175);
+            this.c1DockingTabPage1.TabIndex = 0;
+            this.c1DockingTabPage1.Text = "Page1";
+            // 
+            // c1DateEdit2
+            // 
+            this.c1DateEdit2.AllowSpinLoop = false;
+            this.c1DateEdit2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.c1DateEdit2.Calendar.RightToLeft = System.Windows.Forms.RightToLeft.Inherit;
+            this.c1DateEdit2.Calendar.VisualStyle = C1.Win.C1Input.VisualStyle.Office2007Blue;
+            this.c1DateEdit2.Calendar.VisualStyleBaseStyle = C1.Win.C1Input.VisualStyle.Office2007Blue;
+            this.c1DateEdit2.ImagePadding = new System.Windows.Forms.Padding(0);
+            this.c1DateEdit2.Location = new System.Drawing.Point(37, 145);
+            this.c1DateEdit2.Name = "c1DateEdit2";
+            this.c1DateEdit2.Size = new System.Drawing.Size(200, 18);
+            this.c1DateEdit2.TabIndex = 3;
+            this.c1DateEdit2.Tag = null;
+            this.c1ThemeController1.SetTheme(this.c1DateEdit2, "(default)");
+            this.c1DateEdit2.VisualStyle = C1.Win.C1Input.VisualStyle.Office2007Blue;
+            this.c1DateEdit2.VisualStyleBaseStyle = C1.Win.C1Input.VisualStyle.Office2007Blue;
+            // 
+            // c1Button1
+            // 
+            this.c1Button1.Location = new System.Drawing.Point(37, 116);
+            this.c1Button1.Name = "c1Button1";
+            this.c1Button1.Size = new System.Drawing.Size(75, 23);
+            this.c1Button1.TabIndex = 5;
+            this.c1Button1.Text = "c1Button1";
+            this.c1ThemeController1.SetTheme(this.c1Button1, "(default)");
+            this.c1Button1.UseVisualStyleBackColor = true;
+            // 
+            // c1DateEdit1
+            // 
+            this.c1DateEdit1.AllowSpinLoop = false;
+            this.c1DateEdit1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            // 
+            // 
+            // 
+            this.c1DateEdit1.Calendar.Font = new System.Drawing.Font("Tahoma", 8F);
+            this.c1DateEdit1.Calendar.VisualStyle = C1.Win.C1Input.VisualStyle.Office2007Blue;
+            this.c1DateEdit1.Calendar.VisualStyleBaseStyle = C1.Win.C1Input.VisualStyle.Office2007Blue;
+            this.c1DateEdit1.ImagePadding = new System.Windows.Forms.Padding(0);
+            this.c1DateEdit1.Location = new System.Drawing.Point(37, 92);
+            this.c1DateEdit1.Name = "c1DateEdit1";
+            this.c1DateEdit1.Size = new System.Drawing.Size(200, 18);
+            this.c1DateEdit1.TabIndex = 4;
+            this.c1DateEdit1.Tag = null;
+            this.c1ThemeController1.SetTheme(this.c1DateEdit1, "(default)");
+            this.c1DateEdit1.VisualStyle = C1.Win.C1Input.VisualStyle.Office2007Blue;
+            this.c1DateEdit1.VisualStyleBaseStyle = C1.Win.C1Input.VisualStyle.Office2007Blue;
+            // 
+            // c1TextBox1
+            // 
+            this.c1TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.c1TextBox1.Location = new System.Drawing.Point(37, 68);
+            this.c1TextBox1.Name = "c1TextBox1";
+            this.c1TextBox1.Size = new System.Drawing.Size(100, 18);
+            this.c1TextBox1.TabIndex = 3;
+            this.c1TextBox1.Tag = null;
+            this.c1ThemeController1.SetTheme(this.c1TextBox1, "(default)");
+            this.c1TextBox1.VisualStyle = C1.Win.C1Input.VisualStyle.Office2007Blue;
+            this.c1TextBox1.VisualStyleBaseStyle = C1.Win.C1Input.VisualStyle.Office2007Blue;
+            // 
+            // c1SuperTooltip1
+            // 
+            this.c1SuperTooltip1.Font = new System.Drawing.Font("Tahoma", 8F);
+            this.c1SuperTooltip1.RightToLeft = System.Windows.Forms.RightToLeft.Inherit;
+            this.c1ThemeController1.SetTheme(this.c1SuperTooltip1, "(default)");
+            // 
+            // c1FlexGrid1
+            // 
+            this.c1FlexGrid1.ColumnInfo = "10,1,0,0,0,95,Columns:";
+            this.c1FlexGrid1.Location = new System.Drawing.Point(343, 12);
+            this.c1FlexGrid1.Name = "c1FlexGrid1";
+            this.c1FlexGrid1.Rows.DefaultSize = 19;
+            this.c1FlexGrid1.ShowThemedHeaders = C1.Win.C1FlexGrid.ShowThemedHeadersEnum.None;
+            this.c1FlexGrid1.Size = new System.Drawing.Size(240, 150);
+            this.c1FlexGrid1.TabIndex = 2;
+            this.c1ThemeController1.SetTheme(this.c1FlexGrid1, "(default)");
+            // 
+            // c1SuperErrorProvider1
+            // 
+            this.c1SuperErrorProvider1.ContainerControl = this;
+            this.c1ThemeController1.SetTheme(this.c1SuperErrorProvider1, "(default)");
+            this.c1SuperErrorProvider1.ToolTip = this.c1SuperTooltip1;
+            // 
+            // c1DockingTab2
+            // 
+            this.c1DockingTab2.Controls.Add(this.c1DockingTabPage2);
+            this.c1DockingTab2.Location = new System.Drawing.Point(362, 181);
+            this.c1DockingTab2.Name = "c1DockingTab2";
+            this.c1DockingTab2.Size = new System.Drawing.Size(300, 200);
+            this.c1DockingTab2.TabIndex = 3;
+            this.c1DockingTab2.TabsSpacing = 5;
+            this.c1ThemeController1.SetTheme(this.c1DockingTab2, "(default)");
+            this.c1DockingTab2.VisualStyle = C1.Win.C1Command.VisualStyle.Office2010Blue;
+            this.c1DockingTab2.VisualStyleBase = C1.Win.C1Command.VisualStyle.Office2010Blue;
+            // 
+            // c1DockingTabPage2
+            // 
+            this.c1DockingTabPage2.Location = new System.Drawing.Point(1, 24);
+            this.c1DockingTabPage2.Name = "c1DockingTabPage2";
+            this.c1DockingTabPage2.Size = new System.Drawing.Size(298, 175);
+            this.c1DockingTabPage2.TabIndex = 0;
+            this.c1DockingTabPage2.Text = "Page2";
+            // 
+            // c1CommandDock1
+            // 
+            this.c1CommandDock1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.c1CommandDock1.Id = 1;
+            this.c1CommandDock1.Location = new System.Drawing.Point(0, 0);
+            this.c1CommandDock1.Name = "c1CommandDock1";
+            this.c1CommandDock1.Size = new System.Drawing.Size(32, 450);
+            this.c1ThemeController1.SetTheme(this.c1CommandDock1, "(default)");
+            // 
+            // c1List1
+            // 
+            this.c1List1.AddItemSeparator = ';';
+            this.c1List1.ColumnWidth = 100;
+            this.c1List1.DeadAreaBackColor = System.Drawing.SystemColors.ControlDark;
+            this.c1List1.Images.Add(((System.Drawing.Image)(resources.GetObject("c1List1.Images"))));
+            this.c1List1.ItemHeight = 15;
+            this.c1List1.Location = new System.Drawing.Point(50, 265);
+            this.c1List1.MatchEntryTimeout = ((long)(2000));
+            this.c1List1.Name = "c1List1";
+            this.c1List1.PreviewInfo.Location = new System.Drawing.Point(0, 0);
+            this.c1List1.PreviewInfo.Size = new System.Drawing.Size(0, 0);
+            this.c1List1.PreviewInfo.ZoomFactor = 75D;
+            this.c1List1.PrintInfo.PageSettings = ((System.Drawing.Printing.PageSettings)(resources.GetObject("c1List1.PrintInfo.PageSettings")));
+            this.c1List1.RowDivider.Style = C1.Win.C1List.LineStyleEnum.None;
+            this.c1List1.RowSubDividerColor = System.Drawing.Color.DarkGray;
+            this.c1List1.ShowHeaderCheckBox = false;
+            this.c1List1.Size = new System.Drawing.Size(289, 141);
+            this.c1List1.TabIndex = 4;
+            this.c1List1.Text = "c1List1";
+            this.c1ThemeController1.SetTheme(this.c1List1, "(default)");
+            this.c1List1.VisualStyle = C1.Win.C1List.VisualStyle.Office2010Blue;
+            this.c1List1.PropBag = resources.GetString("c1List1.PropBag");
+            // 
+            // c1DockingManager1
+            // 
+            this.c1DockingManager1.ParentContainer = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.c1List1);
+            this.Controls.Add(this.c1CommandDock1);
+            this.Controls.Add(this.c1DockingTab2);
+            this.Controls.Add(this.c1FlexGrid1);
+            this.Controls.Add(this.c1DockingTab1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.c1Combo1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c1DockingTab1)).EndInit();
+            this.c1DockingTab1.ResumeLayout(false);
+            this.c1DockingTabPage1.ResumeLayout(false);
+            this.c1DockingTabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.c1DateEdit2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c1Button1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c1DateEdit1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c1TextBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c1ThemeController1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c1FlexGrid1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c1SuperErrorProvider1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c1DockingTab2)).EndInit();
+            this.c1DockingTab2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.c1CommandDock1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c1List1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c1DockingManager1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c1ThemeController2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private C1.Win.C1List.C1Combo c1Combo1;
+        private C1.Win.C1Command.C1DockingTab c1DockingTab1;
+        private C1.Win.C1Command.C1DockingTabPage c1DockingTabPage1;
+        private C1.Win.C1Themes.C1ThemeController c1ThemeController1;
+        private C1.Win.C1SuperTooltip.C1SuperTooltip c1SuperTooltip1;
+        private C1.Win.C1FlexGrid.C1FlexGrid c1FlexGrid1;
+        private C1.Win.C1Input.C1TextBox c1TextBox1;
+        private C1.Win.C1Input.C1DateEdit c1DateEdit1;
+        private C1.Win.C1Input.C1Button c1Button1;
+        private C1.Win.C1SuperTooltip.C1SuperErrorProvider c1SuperErrorProvider1;
+        private C1.Win.Calendar.C1DateEdit c1DateEdit2;
+        private C1.Win.C1Command.C1CommandDock c1CommandDock1;
+        private C1.Win.C1Command.C1DockingTab c1DockingTab2;
+        private C1.Win.C1Command.C1DockingTabPage c1DockingTabPage2;
+        private C1.Win.C1Command.C1DockingManager c1DockingManager1;
+        private C1.Win.C1List.C1List c1List1;
+        private C1.Win.C1Themes.C1ThemeController c1ThemeController2;
+    }
+}
+
