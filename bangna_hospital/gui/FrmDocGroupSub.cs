c249ac6d3d7b4c1c56fd54ecb40e8c8ea684854f
Ekapop <eploentham@gmail.com>
2019-01-04 17:34:43 +0700

    1
    1

---
+++ bangna_hospital/gui/FrmDocGroupSub.cs
@@ -0,0 +1,62 @@
+﻿using bangna_hospital.control;
+using C1.Win.C1FlexGrid;
+using C1.Win.C1SuperTooltip;
+using C1.Win.C1Themes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bangna_hospital.gui
+{
+    public partial class FrmDocGroupSub : Form
+    {
+        BangnaControl bc;
+        Font fEdit, fEditB;
+
+        Color bg, fc;
+        Font ff, ffB;
+        int colID = 1, colName = 2;
+
+        C1FlexGrid grfPosi;
+
+        //C1TextBox txtPassword = new C1.Win.C1Input.C1TextBox();
+        Boolean flagEdit = false;
+        C1SuperTooltip stt;
+        C1SuperErrorProvider sep;
+
+        String userIdVoid = "";
+        public FrmDocGroupSub(BangnaControl x)
+        {
+            InitializeComponent();
+            bc = x;
+            initConfig();
+        }
+        private void initConfig()
+        {
+            fEdit = new Font(bc.iniC.grdViewFontName, bc.grdViewFontSize, FontStyle.Regular);
+            fEditB = new Font(bc.iniC.grdViewFontName, bc.grdViewFontSize, FontStyle.Bold);
+
+            C1ThemeController.ApplicationTheme = bc.iniC.themeApplication;
+            theme1.Theme = C1ThemeController.ApplicationTheme;
+            theme1.SetTheme(sB, "BeigeOne");
+            foreach (Control c in panel3.Controls)
+            {
+                theme1.SetTheme(c, "Office2013Red");
+            }
+
+            bg = txtDocGroupSubName.BackColor;
+            fc = txtDocGroupSubName.ForeColor;
+            ff = txtDocGroupSubName.Font;
+        }
+        private void FrmDocGroupSub_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
