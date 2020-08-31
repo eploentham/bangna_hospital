2efd414576be18ff327ab5f6cbe95ae846b000ce
Ekapop <eploentham@gmail.com>
2020-08-31 13:55:37 +0700

    1
    1

---
+++ bangna_hospital/report/xray_result.cs
@@ -0,0 +1,18 @@
+﻿namespace bangna_hospital.report
+{
+}
+
+namespace bangna_hospital.report
+{
+
+
+    public partial class xray_result
+    {
+    }
+}
+namespace bangna_hospital.report {
+    
+    
+    public partial class xray_result {
+    }
+}
