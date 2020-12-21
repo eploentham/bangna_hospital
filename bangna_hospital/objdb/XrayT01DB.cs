39992a24347cf1c54220258ec75243ac9ec51242
Ekapop <eploentham@gmail.com>
2020-12-21 14:05:12 +0700

    1
    1

---
+++ bangna_hospital/objdb/XrayT01DB.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bangna_hospital.objdb
+{
+    class XrayT01DB
+    {
+    }
+}
