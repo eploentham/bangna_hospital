1a386e78e34b144da5b334ea86229d95b673471d
Ekapop <eploentham@gmail.com>
2020-12-21 15:13:58 +0700

    1
    1

--- bangna_hospital/objdb/XrayT01DB.cs
+++ bangna_hospital/objdb/XrayT01DB.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using bangna_hospital.object1;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,58 @@ using System.Threading.Tasks;
 
 namespace bangna_hospital.objdb
 {
-    class XrayT01DB
+    public class XrayT01DB
     {
+        public XrayT01 XrayT01;
+        ConnectDB conn;
+        public XrayT01DB(ConnectDB c)
+        {
+            conn = c;
+            initConfig();
+        }
+        private void initConfig()
+        {
+
+            XrayT01 = new XrayT01();
+            XrayT01.MNC_REQ_YR = "MNC_REQ_YR";
+            XrayT01.MNC_REQ_NO = "MNC_REQ_NO";
+            XrayT01.MNC_REQ_DAT = "MNC_REQ_DAT";
+            XrayT01.MNC_REQ_DEP = "MNC_REQ_DEP";
+            XrayT01.MNC_REQ_STS = "MNC_REQ_STS";
+            XrayT01.MNC_REQ_TIM = "MNC_REQ_TIM";
+            XrayT01.MNC_HN_YR = "MNC_HN_YR";
+            XrayT01.MNC_HN_NO = "MNC_HN_NO";
+            XrayT01.MNC_AN_YR = "MNC_AN_YR";
+            XrayT01.MNC_AN_NO = "MNC_AN_NO";
+            XrayT01.MNC_PRE_NO = "MNC_PRE_NO";
+            XrayT01.MNC_DATE = "MNC_DATE";
+            XrayT01.MNC_TIME = "MNC_TIME";
+            XrayT01.MNC_DOT_CD = "MNC_DOT_CD";
+            XrayT01.MNC_WD_NO = "MNC_WD_NO";
+            XrayT01.MNC_RM_NAM = "MNC_RM_NAM";
+            XrayT01.MNC_BD_NO = "MNC_BD_NO";
+            XrayT01.MNC_FN_TYP_CD = "MNC_FN_TYP_CD";
+            XrayT01.MNC_COM_CD = "MNC_COM_CD";
+            XrayT01.MNC_REM = "MNC_REM";
+            XrayT01.MNC_XR_STS = "MNC_XR_STS";
+            XrayT01.MNC_CAL_NO = "MNC_CAL_NO";
+            XrayT01.MNC_EMPR_CD = "MNC_EMPR_CD";
+            XrayT01.MNC_EMPC_CD = "MNC_EMPC_CD";
+            XrayT01.MNC_ORD_DOT = "MNC_ORD_DOT";
+            XrayT01.MNC_CFM_DOT = "MNC_CFM_DOT";
+            XrayT01.MNC_DOC_YR = "MNC_DOC_YR";
+            XrayT01.MNC_DOC_NO = "MNC_DOC_NO";
+            XrayT01.MNC_DOC_DAT = "MNC_DOC_DAT";
+            XrayT01.MNC_DOC_CD = "MNC_DOC_CD";
+            XrayT01.MNC_STAMP_DAT = "MNC_STAMP_DAT";
+            XrayT01.MNC_STAMP_TIM = "MNC_STAMP_TIM";
+            XrayT01.MNC_CANCEL_STS = "MNC_CANCEL_STS";
+            XrayT01.MNC_PAC_CD = "MNC_PAC_CD";
+            XrayT01.MNC_PAC_TYP = "MNC_PAC_TYP";
+            XrayT01.status_pacs = "status_pacs";
+
+
+
+        }
     }
 }
---
+++ bangna_hospital/objdb/XrayT02DB.cs
@@ -0,0 +1,50 @@
+﻿using bangna_hospital.object1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bangna_hospital.objdb
+{
+    public class XrayT02DB
+    {
+        public XrayT02 XrayT02;
+        ConnectDB conn;
+        public XrayT02DB(ConnectDB c)
+        {
+            conn = c;
+            initConfig();
+        }
+        private void initConfig()
+        {
+
+            XrayT02 = new XrayT02();
+            XrayT02.MNC_REQ_YR = "MNC_REQ_YR";
+            XrayT02.MNC_REQ_NO = "MNC_REQ_NO";
+            XrayT02.MNC_REQ_DAT = "MNC_REQ_DAT";
+            XrayT02.MNC_REQ_STS = "MNC_REQ_STS";
+            XrayT02.MNC_XR_CD = "MNC_XR_CD";
+            XrayT02.MNC_XR_RMK = "MNC_XR_RMK";
+            XrayT02.MNC_XR_COS = "MNC_XR_COS";
+            XrayT02.MNC_XR_PRI = "MNC_XR_PRI";
+            XrayT02.MNC_XR_RFN = "MNC_XR_RFN";
+            XrayT02.MNC_XR_PRI_R = "MNC_XR_PRI_R";
+            XrayT02.MNC_FLG_K = "MNC_FLG_K";
+            XrayT02.MNC_STAMP_DAT = "MNC_STAMP_DAT";
+            XrayT02.MNC_STAMP_TIM = "MNC_STAMP_TIM";
+            XrayT02.MNC_XR_COS_R = "MNC_XR_COS_R";
+            XrayT02.MNC_DOT_CD_DF = "MNC_DOT_CD_DF";
+            XrayT02.MNC_DOT_GRP_CD = "MNC_DOT_GRP_CD";
+            XrayT02.MNC_ACT_DAT = "MNC_ACT_DAT";
+            XrayT02.MNC_ACT_TIM = "MNC_ACT_TIM";
+            XrayT02.MNC_CANCEL_STS = "MNC_CANCEL_STS";
+            XrayT02.MNC_USR_UPD = "MNC_USR_UPD";
+            XrayT02.MNC_SND_OUT_STS = "MNC_SND_OUT_STS";
+            XrayT02.MNC_XR_STS = "MNC_XR_STS";
+            XrayT02.status_pacs = "status_pacs";
+         
+
+        }
+    }
+}
