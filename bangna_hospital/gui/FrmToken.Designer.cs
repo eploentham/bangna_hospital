2fa7ece1c5c4dbe039afbbfef31c7327ed036d7b
Ekapop <eploentham@gmail.com>
2025-02-16 08:15:36 +0700

    1
    1

---
+++ bangna_hospital/gui/FrmToken.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace bangna_hospital.gui
+{
+    partial class FrmToken
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.c1StatusBar1 = new C1.Win.C1Ribbon.C1StatusBar();
+            this.c1DockingTab1 = new C1.Win.C1Command.C1DockingTab();
+            this.c1DockingTabPage1 = new C1.Win.C1Command.C1DockingTabPage();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.c1StatusBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c1DockingTab1)).BeginInit();
+            this.c1DockingTab1.SuspendLayout();
+            this.c1DockingTabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // c1StatusBar1
+            // 
+            this.c1StatusBar1.AutoSizeElement = C1.Framework.AutoSizeElement.Width;
+            this.c1StatusBar1.Location = new System.Drawing.Point(0, 783);
+            this.c1StatusBar1.Name = "c1StatusBar1";
+            this.c1StatusBar1.Size = new System.Drawing.Size(1150, 22);
+            // 
+            // c1DockingTab1
+            // 
+            this.c1DockingTab1.Controls.Add(this.c1DockingTabPage1);
+            this.c1DockingTab1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.c1DockingTab1.Location = new System.Drawing.Point(0, 0);
+            this.c1DockingTab1.Name = "c1DockingTab1";
+            this.c1DockingTab1.Size = new System.Drawing.Size(1150, 783);
+            this.c1DockingTab1.TabIndex = 1;
+            this.c1DockingTab1.TabsSpacing = 5;
+            // 
+            // c1DockingTabPage1
+            // 
+            this.c1DockingTabPage1.Controls.Add(this.panel1);
+            this.c1DockingTabPage1.Location = new System.Drawing.Point(1, 24);
+            this.c1DockingTabPage1.Name = "c1DockingTabPage1";
+            this.c1DockingTabPage1.Size = new System.Drawing.Size(1148, 758);
+            this.c1DockingTabPage1.TabIndex = 0;
+            this.c1DockingTabPage1.Text = "Page1";
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1148, 758);
+            this.panel1.TabIndex = 0;
+            // 
+            // FrmToken
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1150, 805);
+            this.Controls.Add(this.c1DockingTab1);
+            this.Controls.Add(this.c1StatusBar1);
+            this.Name = "FrmToken";
+            this.Text = "FrmToken";
+            ((System.ComponentModel.ISupportInitialize)(this.c1StatusBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c1DockingTab1)).EndInit();
+            this.c1DockingTab1.ResumeLayout(false);
+            this.c1DockingTabPage1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private C1.Win.C1Ribbon.C1StatusBar c1StatusBar1;
+        private C1.Win.C1Command.C1DockingTab c1DockingTab1;
+        private C1.Win.C1Command.C1DockingTabPage c1DockingTabPage1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
